e13c2e5e93a58e92fd59828b99c1422c10a21952
Jotas <jotasimp32@gmail.com>
2019-06-08 17:59:21 +0200

    Fix package issues and track master on library dependency.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "MHW-Discord",
+    products: [
+        .executable(name: "MHW-Discord", targets: ["MHW-Discord"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../MHWBotCore")
+        .package(url: "../MHWBotCore", .branch("master")),
+        .package(url: "https://github.com/Azoy/Sword", .upToNextMajor(from: "0.9.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MHW-Discord",
-            dependencies: ["MHWBotCore"]),
+            dependencies: ["MHWBotCore", "Sword"]),
         .testTarget(
             name: "MHW-DiscordTests",
             dependencies: ["MHW-Discord"]),
